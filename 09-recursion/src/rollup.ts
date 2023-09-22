8540181bdfd05acb394f307304d29c7a91995085
Ryan Soury <ryan@webdoodle.com.au>
2023-09-22 22:28:14 +1000

    experimenting with zk rollups

--- 09-recursion/src/rollup.ts
+++ 09-recursion/src/rollup.ts
@@ -1,60 +1,51 @@
 import {
-  isReady,
+  // isReady,
   shutdown,
   Field,
-  Mina,
-  PrivateKey,
-  AccountUpdate,
+  // Mina,
+  // PrivateKey,
+  // AccountUpdate,
   SelfProof,
   Experimental,
   Struct,
-  Bool,
-  Circuit,
-  Poseidon,
+  // Bool,
+  // Circuit,
+  // Poseidon,
   MerkleMap,
-  MerkleTree,
-  MerkleWitness,
+  // MerkleTree,
+  // MerkleWitness,
   MerkleMapWitness,
   verify,
-  SmartContract,
-  state,
-  State,
-  method,
-  DeployArgs,
+  // SmartContract,
+  // state,
+  // method,
+  // DeployArgs,
   Proof,
-  Permissions,
-} from 'snarkyjs';
+  // Permissions,
+} from 'o1js';
+import { Logger } from 'tslog';
 
-class MerkleWitness20 extends MerkleWitness(20) {}
+// class MerkleWitness20 extends MerkleWitness(20) {}
+
+const logger = new Logger();
 
 // ===============================================================
 
 async function main() {
-  await isReady;
+  logger.info('SnarkyJS loaded');
 
-  console.log('SnarkyJS loaded');
-
-  console.log('compiling...');
+  logger.info('compiling...');
 
   const { verificationKey } = await Rollup.compile();
 
-  console.log('generating transition information');
+  logger.info('generating transition information');
 
-  const transitions = 
-    [
-      { key: Field(8),
-        increment: Field(3),
-      },
-      { key: Field(43),
-        increment: Field(2),
-      },
-      { key: Field(6),
-        increment: Field(3999),
-      },
-      { key: Field(8),
-        increment: Field(400),
-      },
-  ]
+  const transitions = [
+    { key: Field(8), increment: Field(3) },
+    { key: Field(43), increment: Field(2) },
+    { key: Field(6), increment: Field(3999) },
+    { key: Field(8), increment: Field(400) },
+  ];
 
   let map = new MerkleMap();
 
@@ -70,26 +61,83 @@ async function main() {
     map.set(key, updatedValue);
     const latestRoot = map.getRoot();
 
-    rollupStepInfo.push({ initialRoot, latestRoot, key, currentValue, increment, witness });
+    rollupStepInfo.push({
+      initialRoot,
+      latestRoot,
+      key,
+      currentValue,
+      increment,
+      witness,
+    });
   });
 
-  console.log('making first set of proofs');
+  logger.info('making first set of proofs');
 
   // These could all be done in parallel in a real rollup
   // If T is the time to make a proof this could take time T
-  // const rollupProofs = rollupStepInfo.map(async ({ initialRoot, latestRoot, key, currentValue, increment, witness }) => {
-  //   const rollup = RollupState.createOneStep(initialRoot, latestRoot, key, currentValue, increment, witness);
-  //   const proof = await Rollup.oneStep(rollup, initialRoot, latestRoot, key, currentValue, increment, witness);
-  //   return proof;
-  // });
-  const rollupProofs: Proof<RollupState>[] = [];
-  for (var { initialRoot, latestRoot, key, currentValue, increment, witness } of rollupStepInfo) {
-    const rollup = RollupState.createOneStep(initialRoot, latestRoot, key, currentValue, increment, witness);
-    const proof = await Rollup.oneStep(rollup, initialRoot, latestRoot, key, currentValue, increment, witness);
+  // const rollupProofs = rollupStepInfo.map(
+  //   async ({
+  //     initialRoot,
+  //     latestRoot,
+  //     key,
+  //     currentValue,
+  //     increment,
+  //     witness,
+  //   }) => {
+  //     const rollup = RollupState.createOneStep(
+  //       initialRoot,
+  //       latestRoot,
+  //       key,
+  //       currentValue,
+  //       increment,
+  //       witness
+  //     );
+  //     const proof = await Rollup.oneStep(
+  //       rollup,
+  //       initialRoot,
+  //       latestRoot,
+  //       key,
+  //       currentValue,
+  //       increment,
+  //       witness
+  //     );
+  //     return proof;
+  //   }
+  // );
+  const rollupProofs: Proof<RollupState, void>[] = [];
+  for (var {
+    initialRoot,
+    latestRoot,
+    key,
+    currentValue,
+    increment,
+    witness,
+  } of rollupStepInfo) {
+    const rollup = RollupState.createOneStep(
+      initialRoot,
+      latestRoot,
+      key,
+      currentValue,
+      increment,
+      witness
+    );
+    logger.info(`rollup updated: ${rollup.latestRoot}`);
+    const proof = await Rollup.oneStep(
+      rollup,
+      initialRoot,
+      latestRoot,
+      key,
+      currentValue,
+      increment,
+      witness
+    );
+    logger.info(`proof created: ${proof.publicInput.latestRoot}`);
     rollupProofs.push(proof);
   }
 
-  console.log('merging proofs');
+  // const rps = await Promise.all(rollupProofs);
+  const rps = rollupProofs;
+  logger.info('merging proofs');
 
   // These could also all be done in parallel in a real rollup
   // If T is the time to make a proof this could take time log(n)*T
@@ -97,55 +145,61 @@ async function main() {
   //   const rollup = RollupState.createMerged((await a).publicInput, (await b).publicInput);
   //   return await Rollup.merge(rollup, (await a), (await b));
   // });
-  var proof: Proof<RollupState> = rollupProofs[0];
-  for (let i=1; i<rollupProofs.length; i++) {
-    const rollup = RollupState.createMerged(proof.publicInput, rollupProofs[i].publicInput);
-    let mergedProof = await Rollup.merge(rollup, proof, rollupProofs[i]);
+  var proof: Proof<RollupState, void> = await rps[0];
+  for (let i = 1; i < rollupProofs.length; i++) {
+    const rollup = RollupState.createMerged(
+      proof.publicInput,
+      rps[i].publicInput
+    );
+    let mergedProof = await Rollup.merge(rollup, proof, rps[i]);
     proof = mergedProof;
   }
 
-  console.log('verifying rollup');
-  console.log(proof.publicInput.latestRoot.toString());
+  logger.info('verifying rollup');
+  logger.info(proof.publicInput.latestRoot.toString());
 
   const ok = await verify(proof.toJSON(), verificationKey);
-  console.log('ok', ok);
+  logger.info('ok', ok);
 
-  console.log('Shutting down');
+  logger.info('Shutting down');
 
   await shutdown();
-};
+}
 
 // ===============================================================
 
 class RollupState extends Struct({
   initialRoot: Field,
   latestRoot: Field,
-}) { 
-
+}) {
   static createOneStep(
-    initialRoot: Field, 
-    latestRoot: Field, 
-    key: Field, 
+    initialRoot: Field,
+    latestRoot: Field,
+    key: Field,
     currentValue: Field,
-    incrementAmount: Field, 
-    merkleMapWitness: MerkleMapWitness,
+    incrementAmount: Field,
+    merkleMapWitness: MerkleMapWitness
   ) {
-    const [ witnessRootBefore, witnessKey ] = merkleMapWitness.computeRootAndKey(currentValue);
+    const [witnessRootBefore, witnessKey] =
+      merkleMapWitness.computeRootAndKey(currentValue);
     initialRoot.assertEquals(witnessRootBefore);
     witnessKey.assertEquals(key);
-    const [ witnessRootAfter, _ ] = merkleMapWitness.computeRootAndKey(currentValue.add(incrementAmount));
+    // eslint-disable-next-line
+    const [witnessRootAfter, _] = merkleMapWitness.computeRootAndKey(
+      currentValue.add(incrementAmount)
+    );
     latestRoot.assertEquals(witnessRootAfter);
 
     return new RollupState({
       initialRoot,
-      latestRoot
+      latestRoot,
     });
   }
 
   static createMerged(state1: RollupState, state2: RollupState) {
     return new RollupState({
       initialRoot: state1.initialRoot,
-      latestRoot: state2.latestRoot
+      latestRoot: state2.latestRoot,
     });
   }
 
@@ -162,46 +216,47 @@ const Rollup = Experimental.ZkProgram({
 
   methods: {
     oneStep: {
-      privateInputs: [ Field, Field, Field, Field, Field, MerkleMapWitness ],
+      privateInputs: [Field, Field, Field, Field, Field, MerkleMapWitness],
 
       method(
-        state: RollupState, 
-        initialRoot: Field, 
-        latestRoot: Field, 
-        key: Field, 
+        state: RollupState,
+        initialRoot: Field,
+        latestRoot: Field,
+        key: Field,
         currentValue: Field,
-        incrementAmount: Field, 
+        incrementAmount: Field,
         merkleMapWitness: MerkleMapWitness
       ) {
         const computedState = RollupState.createOneStep(
-          initialRoot, 
-          latestRoot, 
-          key, 
-          currentValue, 
-          incrementAmount, 
+          initialRoot,
+          latestRoot,
+          key,
+          currentValue,
+          incrementAmount,
           merkleMapWitness
         );
         RollupState.assertEquals(computedState, state);
-      }
+      },
     },
 
     merge: {
-      privateInputs: [ SelfProof, SelfProof ],
+      privateInputs: [SelfProof, SelfProof],
 
       method(
         newState: RollupState,
-        rollup1proof: SelfProof<RollupState>,
-        rollup2proof: SelfProof<RollupState>,
+        rollup1proof: SelfProof<RollupState, void>,
+        rollup2proof: SelfProof<RollupState, void>
       ) {
         rollup1proof.verify();
         rollup2proof.verify();
 
-        rollup2proof.publicInput.initialRoot.assertEquals(rollup1proof.publicInput.latestRoot);
+        rollup2proof.publicInput.initialRoot.assertEquals(
+          rollup1proof.publicInput.latestRoot
+        );
         rollup1proof.publicInput.initialRoot.assertEquals(newState.initialRoot);
         rollup2proof.publicInput.latestRoot.assertEquals(newState.latestRoot);
-      }
-    }
-
+      },
+    },
   },
 });
 
@@ -210,32 +265,32 @@ export class RollupProof extends RollupProof_ {}
 
 // ===============================================================
 
-class RollupContract extends SmartContract {
-  @state(Field) state = State<Field>();
+// class RollupContract extends SmartContract {
+//   @state(Field) state = State<Field>();
 
-  deploy(args: DeployArgs) {
-    super.deploy(args);
-    this.setPermissions({
-      ...Permissions.default(),
-      editState: Permissions.proofOrSignature(),
-    });
-  }
+//   deploy(args: DeployArgs) {
+//     super.deploy(args);
+//     this.setPermissions({
+//       ...Permissions.default(),
+//       editState: Permissions.proofOrSignature(),
+//     });
+//   }
 
-  @method initStateRoot(stateRoot: Field) {
-    this.state.set(stateRoot);
-  }
+//   @method initStateRoot(stateRoot: Field) {
+//     this.state.set(stateRoot);
+//   }
 
-  @method update(rollupStateProof: RollupProof) {
-    const currentState = this.state.get();
-    this.state.assertEquals(currentState);
+//   @method update(rollupStateProof: RollupProof) {
+//     const currentState = this.state.get();
+//     this.state.assertEquals(currentState);
 
-    rollupStateProof.publicInput.initialRoot.assertEquals(currentState);
+//     rollupStateProof.publicInput.initialRoot.assertEquals(currentState);
 
-    rollupStateProof.verify();
+//     rollupStateProof.verify();
 
-    this.state.set(rollupStateProof.publicInput.latestRoot);
-  }
-}
+//     this.state.set(rollupStateProof.publicInput.latestRoot);
+//   }
+// }
 
 // ===============================================================
 
